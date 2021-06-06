89e84b8db299bb2c4421e4550f103ec6d67bb5cb
ayoubechchetyouy <ayoubechchetyouy19@gmail.com>
2021-06-06 22:55:05 +0100

    video 21

--- test.py
+++ test.py
@@ -1,24 +1,14 @@
 from tkinter import *
 from tkinter import ttk
 
-def resize(ev=None):
-    label.configure(font='Aria -%d bold' %scale.get())
+def shortcut(action):
+    print(action)
+window = Tk()
 
-root = Tk()
-root.geometry('500x500')
-
-label = ttk.Label(root, text='hello tkinter')
-label.pack(fill = 'y', expand=1)
-label.config(foreground='silver', background='brown')
-
-scale = ttk.Scale(root, from_=10,to=70,command=resize)
-scale.set(20)
-scale.pack()
-
-quit = ttk.Button(root,text='quit', command=root.quit)
-quit.pack()
+window.bind('<Control-c>',lambda e:shortcut('Copy'))
+window.bind('<Control-v>',lambda e:shortcut('paste'))
+window.bind('<Control-x>',lambda e:shortcut('cut'))
+window.bind('<Control-s>',lambda e:shortcut('save'))
 
 
-
-
-root.mainloop()
+window.mainloop()
