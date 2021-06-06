b69504b6229146c04c60c0da0a6c0115cfd8897a
ayoubechchetyouy <ayoubechchetyouy19@gmail.com>
2021-06-06 16:48:29 +0100

    Initial commit

---
+++ test.py
@@ -0,0 +1,17 @@
+from tkinter import *
+from tkinter import ttk
+win = Tk()
+
+button = ttk.Button(win, text='Click me')
+button.pack()
+
+def callback():
+    print('python')
+
+button.config(command = callback)
+
+logo = PhotoImage(file='1.png')
+
+button.config(image=logo)
+
+win.mainloop()
