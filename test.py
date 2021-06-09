40271282892b238214a4eb99e90d7912cb718ff1
ayoubechchetyouy <ayoubechchetyouy19@gmail.com>
2021-06-09 23:53:51 +0100

    video 17

--- test.py
+++ test.py
@@ -1,22 +1,35 @@
 from tkinter import *
-from tkinter import ttk
-
-root = Tk()
-note = ttk.Notebook(root)
-
-t1 = Frame(note)
-t2 = Frame(note)
-t3 = Frame(note)
-ttk.Button(t1, text='Exit', command=root.destroy).pack(padx=100, pady=100)
-ttk.Button(t2, text='Exit', command=root.destroy).pack(padx=100, pady=100)
-ttk.Button(t3, text='Exit', command=root.destroy).pack(padx=100, pady=100)
-
-
-note.add(t1, text='Tab One')
-note.add(t2, text='Tab Two')
-note.add(t3, text='Tab Three')
-
-note.pack()
+from functools import partial
+from tkinter import messagebox
+tkWindow = Tk()
+tkWindow.geometry('400x150')
+tkWindow.title('Admin login')
+def validateLogin(username, password):
+	a = username.get()
+	b = password.get()
+	if a == 'admin' and b == 'admin':
+		newWindow = Toplevel(tkWindow)
+		newWindow.title("Profile")
+		newWindow.geometry("200x200")
+		Label(newWindow,text="Welcome").pack()
+	else:
+		messagebox.showwarning("Error", "user not found")
+	return
 
 
-root.mainloop()
+
+usernameLabel = Label(tkWindow, text="User Name").grid(row=0, column=0)
+username = StringVar()
+usernameEntry = Entry(tkWindow, textvariable=username).grid(row=0, column=1)
+
+passwordLabel = Label(tkWindow,text="Password").grid(row=1, column=0)
+password = StringVar()
+passwordEntry = Entry(tkWindow, textvariable=password, show='*').grid(row=1, column=1)
+
+validateLogin = partial(validateLogin, username, password)
+
+loginButton = Button(tkWindow, text="Login",command=validateLogin).grid(row=4, column=0)
+
+
+
+tkWindow.mainloop()
