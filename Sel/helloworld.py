7c3e953026269d8d319ed57734bb4028c2025ca3
ayoubechchetyouy <ayoubechchetyouy19@gmail.com>
2021-06-11 15:52:03 +0100

    sel

--- Sel/helloworld.py
+++ Sel/helloworld.py
@@ -1,14 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium import webdriver
 import time
 driver = webdriver.Chrome()
 driver.get("https://www.python.org")
 print(driver.title)
-search_bar = driver.find_element_by_name("q")
-search_bar.clear()
-search_bar.send_keys("getting started with python")
-search_bar.send_keys(Keys.RETURN)
-print(driver.current_url)
 time.sleep(10)
 driver.close()
