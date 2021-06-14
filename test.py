302c9761798ccbb12a33956b41ab34215cbfb596
ayoubechchetyouy <ayoubechchetyouy19@gmail.com>
2021-06-14 19:24:27 +0100

    Version 1

--- test.py
+++ test.py
@@ -1,33 +1,37 @@
 from tkinter import *
-from functools import partial
-from tkinter import *
 from time import strftime
+from datetime import time
 import tkinter as tk
 root = Tk()
 root.title('Masjid')
 root.geometry('4800x4800')
 root.configure(bg='Black')
-header = Frame(height=100, width=2000, bg='grey').place(x=0,y=0)
+header = Frame(height=100, width=2000, bg='#4a5658').place(x=0,y=0)
 
 #Clock
 def time():
-    clock = strftime('%A:%M:%S %p')
+    clock = strftime('%H:%M:%S %p')
     time_clock.config(text=clock)
     time_clock.after(1000, time)
-time_clock = Label(root, font=('calibri', 40, 'bold'),background='grey',foreground='white')
-time_clock.pack()
+
+
+time_clock = Label(root, font=('calibri', 45, 'bold'),background='#4a5658',foreground='white')
+time_clock.pack(anchor='center')
 time()
 
-#Date
+#Clock
 def Date():
-    date = strftime('%A:%M:%S %p')
-    time_clock.config(text=date)
-    time_clock.after(1000, time)
-time_date = Label(root, font=('calibri', 30, 'bold'),background='grey',foreground='white')
-time_date.pack()
-time()
+    clock = strftime('%a:%b:%Y')
+    time_Date.config(text=clock)
+    time_Date.after(1000, time)
 
 
+time_Date = Label(root, font=('calibri', 25,),background='#4a5658',foreground='white')
+time_Date.place(x=0,y=0)
+Date()
+
+
+FajrFrame = Frame(height=80, width=2000, bg='#8a979a').pack()
 
 
 mainloop()
