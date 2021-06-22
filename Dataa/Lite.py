466f07733462f718ebc3c5e8df1896165e8c10d6
ayoubechchetyouy <ayoubechchetyouy19@gmail.com>
2021-06-22 14:48:58 +0100

    sqlite

--- Dataa/Lite.py
+++ Dataa/Lite.py
@@ -8,10 +8,10 @@ cr.execute("create table if not exists users (user_id integer, name text)")
 cr.execute("create table if not exists skills (name text, progress integer, user_id integer)")
 
 
-cr.execute("insert into users(user_id, name) values(1, 'ayoub')")
-cr.execute("insert into users(user_id, name) values(2, 'ahmed')")
-cr.execute("insert into users(user_id, name) values(3, 'youssef')")
+my_list=['samsung', 'xiaomi', 'iphone', 'lg', 'sony', 'huawei', 'realme']
 
+for key, phone in enumerate(my_list):
+    cr.execute(f"insert into users(user_id, name) values({key}, '{phone}')")
 
 
 db.commit()
