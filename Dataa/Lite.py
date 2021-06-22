2839177516fde7a99cf9206c5d5ec1c7d4f4f06c
ayoubechchetyouy <ayoubechchetyouy19@gmail.com>
2021-06-22 14:35:08 +0100

    sqlite

--- Dataa/Lite.py
+++ Dataa/Lite.py
@@ -1,4 +1,18 @@
 import sqlite3
 db = sqlite3.connect("app.db")
 
-db.execute("create table skills (name text, progress integer, user_id integer )")
+
+cr = db.cursor()
+
+cr.execute("create table if not exists users (user_id integer, name text)")
+cr.execute("create table if not exists skills (name text, progress integer, user_id integer)")
+
+
+cr.execute("insert into users(user_id, name) values(1, 'ayoub')")
+cr.execute("insert into users(user_id, name) values(2, 'ahmed')")
+cr.execute("insert into users(user_id, name) values(3, 'youssef')")
+
+
+
+db.commit()
+db.close()
