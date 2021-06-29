45f795162a3a53f0d81d4665e44a2ce13548db59
ayoubechchetyouy <ayoubechchetyouy19@gmail.com>
2021-06-29 07:32:51 +0100

    new update

--- Dataa/Lite.py
+++ Dataa/Lite.py
@@ -16,11 +16,19 @@ def add_skills():
     print('Add Skill')
 def delete_skills():
     print('Delete Skill')
-def delete_skills():
-    print('Delete Skill')
+def update_skills():
+    print('Update Skill')
 
 
 if user_input in cammands_list:
     print(f'Command Found {user_input}')
+    if user_input == "s":
+        show_skills()
+    elif user_input == "a":
+        add_skills()
+    elif user_input == "d":
+        update_skills()
+    elif user_input == "u":
+        update_skills()
 else:
     print(f'Sorry This Command {user_input} Is Not Found')
