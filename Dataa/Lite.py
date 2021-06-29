4e0eb874fc5f7f4f70f071acfe537402aae13bb6
ayoubechchetyouy <ayoubechchetyouy19@gmail.com>
2021-06-29 23:37:10 +0100

    commit and Save

--- Dataa/Lite.py
+++ Dataa/Lite.py
@@ -14,7 +14,7 @@ input_message = """
 What Do You Want T o DO ?
 "s" => Show All Skills
 "a" => Add New Skills
-"s" => Delete A Skills
+"d" => Delete A Skills
 "u" => Update Skills Progress
 "q" => Quit The App
 Choose Option: 
@@ -24,27 +24,36 @@ cammands_list = ["s", "a", "d", "u", "q"]
 
 def show_skills():
     print('Show Skills')
+    commint_and_close()
+
 def add_skills():
-    print('Add Skill')
+    sk = input("Write Skill Name: ").strip().capitalize()
+    prog = input("Write Skill Progress: ").strip()
+    cr.execute(f"insert into skills(name, progress, user_id) values('{sk}', '{prog}', '{uid}')")
+    commint_and_close()
+
 def delete_skills():
-    print('Delete Skill')
+    sk = input("Write Skill Name: ").strip().capitalize()
+    cr.execute(f"delete from skills where name = {sk} ")
+    commint_and_close()
+
 def update_skills():
     print('Update Skill')
+    commint_and_close()
 
 if user_input in cammands_list:
     print(f'Command Found {user_input}')
     if user_input == "s":
         show_skills()
-        commint_and_close()
+
     elif user_input == "a":
         add_skills()
-        commint_and_close()
+
     elif user_input == "d":
-        update_skills()
-        commint_and_close()
+        delete_skills()
+
     elif user_input == "u":
         update_skills()
-        commint_and_close()
     else:
         print("App Is Closed")
 else:
